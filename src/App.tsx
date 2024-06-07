8d219538575e3b8bce8c16055bf24589925a1602
cindyp <cindya1004@gmail.com>
2024-06-07 13:15:38 -0700

    simplified app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import Player from './components/Player'
 import './App.css'
 import Feed from './components/feed/Feed'
 // import MyTabs from './components/BottomNav';
-import Settings from './components/settings/Settings';
 import NavBar from './components/navbar/NavBar';
 
 function App() {
@@ -13,7 +12,6 @@ function App() {
   return (
     <>
       <Player />
-      <Feed />
       <NavBar />
     </>
   )
